dc12c8665f75c1663c76273ef61966f98bf70d35
jyoung-man <jyp13.jyp@gmail.com>
2025-11-16 13:25:50 +0900

    Repository 항목 UI 구현

--- Daou-Github-Search/Features/SearchRow.swift
+++ Daou-Github-Search/Features/SearchRow.swift
@@ -11,19 +11,55 @@ struct SearchRow: View {
     var repository: Repository
     
     var body: some View {
-        HStack {
-            Text(repository.name)
-            Spacer()
+        VStack(alignment: .leading, spacing: 12) {
             
-            Image(systemName: "star.fill")
-                .foregroundStyle(.yellow)
-            
+            HStack {
+                Text(repository.name)
+                    .font(.title3)
+                    .foregroundStyle(.blue)
+                    .bold()
+                Spacer()
+                
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+            }
+            .padding(.leading, 12)
+            .padding(.trailing, 12)
+
+            HStack {
+                if let description = repository.description {
+                    Text(description)
+                        .foregroundColor(.secondary)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 12) // 좌측 여백
+                }
+            }
+            HStack {
+                if let language = repository.language {
+                    Text(language)
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                }
+                
+                if let license = repository.license?.name {
+                    Text("· \(license)")
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                }
+                
+                if let starCount = repository.starCount {
+                    Text("· \(String(describing: starCount))")
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                }
+            }
+            .padding(.leading, 12) // 좌측 여백
         }
     }
 }
 
 #Preview {
-    let repo = Repository(id: 123213213213, name: "test", fullName: "TEST repository", description: "test repository", starCount: 500, language: "Swift", owner: Owner(name: "jyoung-man", avatarUrl: nil), license: nil, isStarred: true)
+    let repo = Repository(id: 123213213213, name: "test", fullName: "TEST repository", description: "test repository description", starCount: 500, language: "Swift", owner: Owner(name: "jyoung-man", avatarUrl: nil), license: License(key: "123213", name: "MIT", spdxId: "123124124"), isStarred: true)
     return Group {
         SearchRow(repository: repo)
     }
